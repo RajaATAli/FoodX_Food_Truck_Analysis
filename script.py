9063204c0ee74660747f66848ea4aeb0799bf490
RajaATAli <allmdararslan13@gmail.com>
2023-10-16 21:56:36 -0400

    Solved max_results while loop problem

--- script.py
+++ script.py
@@ -3,6 +3,10 @@ import pandas as pd
 import time
 import os
 import dotenv
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
 
 # Load .env file
 dotenv.load_dotenv("APIKEY.env")
@@ -15,7 +19,7 @@ if not API_KEY:
 # Initialize the client with your API key
 gmaps = googlemaps.Client(key=API_KEY)
 
-def get_food_trucks_data(query, location="Indiana, USA", max_results=2):
+def get_food_trucks_data(query, location="Indiana, USA", max_results=3):
     try:
         results = gmaps.places(query=query, location=location, type='restaurant')
         food_trucks = results.get('results', [])
@@ -25,10 +29,13 @@ def get_food_trucks_data(query, location="Indiana, USA", max_results=2):
             next_page_token = results['next_page_token']
             results = gmaps.places(query=query, location=location, type='restaurant', page_token=next_page_token)
             food_trucks.extend(results.get('results', []))
+        
+        # Trim the results to max_results
+        food_trucks = food_trucks[:max_results]
 
         return food_trucks
     except Exception as e:
-        print(f"Error fetching food truck data: {e}")
+        logging.error(f"Error fetching food truck data: {e}")
         return []
 
 def determine_cuisine(name):
@@ -37,6 +44,9 @@ def determine_cuisine(name):
         'BBQ': ['bbq', 'barbecue', 'ribs'],
         'Seafood': ['seafood', 'fish', 'lobster', 'shrimp'],
         'Pizza': ['pizza', 'pizzeria'],
+        'Asian': ['asian', 'chinese', 'japanese', 'korean', 'thai'],
+        'Indian': ['indian', 'curry', 'masala'],
+        'Italian': ['italian', 'pasta', 'spaghetti']
     }
     
     for cuisine, keywords in cuisines.items():
@@ -51,11 +61,15 @@ def get_opening_hours(place_id):
         opening_hours = place_details.get('result', {}).get('opening_hours', {}).get('weekday_text', [])
         return '\n'.join(opening_hours)
     except Exception as e:
-        print(f"Error fetching opening hours for place ID {place_id}: {e}")
+        logging.error(f"Error fetching opening hours for place ID {place_id}: {e}")
         return "Unknown"
 
 def extract_food_truck_info(data):
     df = pd.DataFrame(data)
+    
+    # Check for duplicates
+    df.drop_duplicates(subset='place_id', keep='first', inplace=True)
+    
     df['Cuisine Type'] = df['name'].apply(determine_cuisine)
     if 'website' not in df.columns:
         df['website'] = 'null'
@@ -66,11 +80,9 @@ def extract_food_truck_info(data):
     df_cleaned = df[columns]
     df_cleaned.columns = ['Name', 'Address', 'Rating', 'Website', 'Opening Hours', 'Cuisine Type']
     df_cleaned.to_csv('food_trucks.csv', index=False)
-    print("Data saved to food_trucks.csv")
+    logging.info("Data saved to food_trucks.csv")
 
 if __name__ == "__main__":
     food_trucks_data = get_food_trucks_data(query="food trucks in Indiana")
     if food_trucks_data:
         extract_food_truck_info(food_trucks_data)
-
-
